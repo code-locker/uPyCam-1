01bb46f3047392040e78f8cfe80b4564a2a4e052
code-locker <abhikunjathodi@gmail.com>
2024-08-15 20:42:51 +0530

    timelapse-camera
    Signed-off-by: code-locker <abhikunjathodi@gmail.com>

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-# uPyCam - Streaming 
-This repository includes an application example to stream video using MicroPython on the:
+# uPyCam - Timelapse 
+This repository includes an application example to take photos using MicroPython on the:
 * [ESP32-CAM](https://bit.ly/2Ndn8tN)
 * [M5Camera](https://bit.ly/317Xb74)
 
-Using a browser, you can connect to the boards to see the video stream. 
+The photo can be saved on a microSD (ESP32-CAM) or sent it to an MQTT Broker (M5Camera).
 
-## Timelapse
-The boards can be used as portable timelapse cameras. Check out this repository for more code: [lemariva/uPyCam branch timelapse-camera](https://github.com/lemariva/uPyCam/tree/timelapse-camera)
+## Timelapse videos
+
+### ESP32-CAM
+[![Portable time-lapse camera using an ESP32-CAM running MicroPython](https://img.youtube.com/vi/qW25Bx1nzLo/0.jpg)](https://www.youtube.com/watch?v=qW25Bx1nzLo)
+
+### M5Camera
+[![Timelapse video using MicroPython and an M5Camera](https://img.youtube.com/vi/Tg4jIyBU5zE/0.jpg)](https://www.youtube.com/watch?v=Tg4jIyBU5zE)
 
 ## Firmware
 The MicroPython firmware was extended to add camera support. The firmware is located in this repository: [lemariva/micropython-camera-driver](https://github.com/lemariva/micropython-camera-driver).
@@ -16,10 +21,3 @@ Follow these articles to get more information:
 * [MicroPython: M5CAMERA timelapse over MQTT](https://lemariva.com/blog/2020/06/micropython-m5camera-timelapse-over-mqtt)
 
 Note: If they are not available, they will be published in the next days.
-
-## Web-browser
-You can choose between a photo or streaming mode:
-* Photo mode: `http://<<board-ip>>`
-* Streaming mode: `http://<<board-ip>>/?stream=true`
-
-Streaming mode added by [Krayon](https://github.com/krayon/upyesp32cam/commit/8b63edec50dca9416bb4b2b75207ac53788c597a). Thanks! 
--- main.py
+++ main.py
@@ -1,12 +1,97 @@
-"""
-import webcam
-webcam.run()
-"""
+# Copyright 2020 LeMaRiva|tech lemariva.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import uos
+import machine 
+
+import ntptime
+import time
 import camera
-import machine
-from config import app_config
-from webserver import webcam
+from ftp import ftpserver
+from config import *
 
-server = webcam()
-server.run(app_config)
+if  app_config['mode'] == 'MQTT':
+    from umqtt.simple2 import MQTTClient
+
+try:
+    # camera init
+    led = machine.Pin(app_config['led'], machine.Pin.OUT)
+
+    if app_config['camera'] == 'ESP32-CAM':
+        camera.init(0, format=camera.JPEG, fb_location=camera.PSRAM)  # ESP32-CAM
+    elif app_config['camera'] == 'M5CAMERA':
+        camera.init(0, d0=32, d1=35, d2=34, d3=5, d4=39, d5=18, d6=36, d7=19,
+                    href=26, vsync=25, reset=15, sioc=23, siod=22, xclk=27, pclk=21, fb_location=camera.PSRAM)   #M5CAMERA
+    
+    if app_config['mode'] == 'microSD':
+        # sd mount
+        sd = machine.SDCard(slot=3, width=1, 
+                            sck=machine.Pin(microsd_config['sck']),
+                            mosi=machine.Pin(microsd_config['mosi']),
+                            miso=machine.Pin(microsd_config['miso']),
+                            cs=machine.Pin(microsd_config['ss']))
+        uos.mount(sd, '/sd')
+        #uos.listdir('/')
+    elif  app_config['mode'] == 'MQTT':
+        c = MQTTClient(mqtt_config['client_id'], mqtt_config['server'])
+        c.connect()
+
+    # ntp sync for date
+    ntptime.settime()
+    rtc = machine.RTC()
+
+    if app_config['ftp']:
+        # ftp for checking
+        u = ftpserver()
+        u.start_thread()
+
+except Exception as e:
+    print("Error ocurred: " + str(e))
+    time.sleep_ms(5000)
+    machine.reset()
+
+error_counter = 0
+loop = True
+while loop:
+    try:
+        # prepare for photo
+        led.value(1)
+        led.value(0)
+
+        # take photo
+        buf = camera.capture()
+        # save photo
+        timestamp = rtc.datetime()
+        time_str = '%4d%02d%02d%02d%02d%02d' %(timestamp[0], timestamp[1], timestamp[2], timestamp[4], timestamp[5], timestamp[6])
+
+        if app_config['mode'] == 'microSD':
+            f = open('sd/'+time_str+'.jpg', 'w')
+            f.write(buf)
+            time.sleep_ms(100)
+            f.close()
+        elif  app_config['mode'] == 'MQTT':
+            c.publish(mqtt_config['topic'], buf)
+
+        # sleep
+        time.sleep_ms(app_config['sleep-ms'])
+    
+    except KeyboardInterrupt:
+        print("debugging stopped")
+        loop = False
+
+    except Exception as e:
+        print("Error ocurred: " + str(e))
+        error_counter = error_counter + 1
+        if error_counter > app_config['max-error']:
+            machine.reset()
